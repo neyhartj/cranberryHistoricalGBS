4b1ab923d9ce8b7406e94e609f38d06f10b30175
Jeffrey Neyhart <jeffrey.neyhart@ceres19-compute-60.scinet.local>
2021-03-08 15:47:40 -0600

    edits to some scripts and paths

--- testing/tassel_pipeline/run_GBSSeqToTagDBPlugin_testing.sh
+++ testing/tassel_pipeline/run_GBSSeqToTagDBPlugin_testing.sh
@@ -17,21 +17,28 @@ module load tassel5
 ## Set variables
 # Working directory
 WD=/project/cranberrygbs/cranberryHistoricalGBS/testing/
-# Name of input directory with fastqs
-INPUT=$WD/input/fastq_files
+# Name of input directory
+INPUT=$WD/input
+# Name of input directory with FASTQ
+FASTQDIR=$INPUT/fastq_files
 # Name of database
 DBNAME=$WD/database/cranberry_gbs_discovery.db
 # Name of keyfile
-KEY=$INPUT/cranberry_gbs_all_keys.txt
+# KEY=$INPUT/cranberry_gbs_all_keys.txt
+KEY=$INPUT/C8BNLANXX_1_keyfile.txt
 
+
+# Change working directory
+cd $WD
 
 
 # Execute the plugin
-./run_pipeline.pl -Xms1G -Xmx4G -fork1 -GBSSeqToTagDBPlugin \
+run_pipeline.pl -Xms1G -Xmx4G -fork1 -GBSSeqToTagDBPlugin \
 -c 10 \
 -db $DBNAME \
 -i $INPUT \
 -k $KEY \
+-e EcoT22I \
 -kmerLength 64 \
 -minKmerL 20 \
 -mnQS 20 \
