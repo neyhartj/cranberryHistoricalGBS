f6de2accd0af2415ca6f8e0d7d87a32e4e15d978
Neyhart <12001004083901@FEDIDCARD.GOV>
2021-03-10 08:56:11 -0500

    edits to gzip script

--- input/run_gzip.sh
+++ input/run_gzip.sh
@@ -3,12 +3,12 @@
 # SLURM parameters
 # job standard output will go to the file slurm-%j.out (where %j is the job ID)
 
-#SBATCH --time=04:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --time=08:00:00   # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=2   # 1 processor core(s) per node X 2 threads per core
 #SBATCH --mem=6400M   # maximum memory per node
 #SBATCH --partition=short    # standard node(s)
-#SBATCH --job-name="GBSSeqToTagDBPlugin_testing"
+#SBATCH --job-name="gzip fastq"
 #SBATCH --mail-user=jeffrey.neyhart@usda.gov   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -25,37 +25,22 @@ set -e
 set -u
 set -o pipefail
 
-# Load the TASSEL module
-module load tassel5
+# Directory containing fastq files
+FASTQDIR=/project/cranberrygbs/cranberryHistoricalGBS/input/fastq_files
 
-## Set variables
-# Working directory
-WD=/project/cranberrygbs/cranberryHistoricalGBS/testing/
-# Name of input directory
-INPUT=$WD/input
-# Name of input directory with FASTQ
-FASTQDIR=$INPUT/fastq_files
-# Name of database
-DBNAME=$WD/database/cranberry_gbs_discovery.db
-# Name of keyfile
-# KEY=$INPUT/cranberry_gbs_all_keys.txt
-KEY=$INPUT/C8BNLANXX_1_keyfile.txt
+# Change into that directory
+cd $FASTQDIR
 
+# Search for fastq files that are not compressed
+files=$(find . -name "*_fastq")
 
-# Change working directory
-cd $WD
+# Loop over files
+for fi in $files; do
+  # gzip
+  gzip $fi
+  
+done
 
 
-# Execute the plugin
-run_pipeline.pl -Xms1G -Xmx4G -fork1 -GBSSeqToTagDBPlugin \
--c 10 \
--db $DBNAME \
--i $INPUT \
--k $KEY \
--e EcoT22I \
--kmerLength 64 \
--minKmerL 20 \
--mnQS 20 \
--mxKmerNum 100000000 \
--endPlugin -runfork1
+
 
