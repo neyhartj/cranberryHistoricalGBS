ff562b766a39a9235d22b1786640d1693018ab26
neyhartj <neyhartje@gmail.com>
2021-12-29 08:10:23 -0500

    Added plink haplotyping script.

--- scripts/08_run_plink_stats.sh
+++ scripts/08_run_plink_stats.sh
@@ -12,11 +12,12 @@
 
 
 # Load the modules
-module load plink/0.1.9
+module load plink/1.9
+module load vcftools
 
 ## Set variables
 # Working directory
-WD=/project/cranberrygbs/cranberryHistoricalGBS/
+WD=/project/gifvl_vaccinium/cranberrygbs/cranberryHistoricalGBS/
 # Name of input directory
 INPUT=$WD/imputation/beagle_imputation/
 # Output directory
@@ -25,35 +26,27 @@ OUTPUT=$WD/haplotyping
 # Name of input vcf
 VCFIN=$INPUT/cranberryGBS_germplasm_imputed_snps.vcf.gz
 
-
 # Change working directory
 cd $WD 
 
 
-## Filter
 
-# Filter to retain the germplasm collection, then filter SNPs for excessive 
-# missingness (> 50%) and then filter individuals for excessive missingness (> 50%)
-vcftools --gzvcf $VCFIN --keep $WD/input/cranberry_gbs_germplasm_collection_individuals.txt \
---recode-INFO-all --recode --stdout | \
-vcftools --vcf - --mac 15 --max-missing 0.5 --recode-INFO-all --recode --out $OUTPUT/cranberryGBS_snps_to_impute
-
-# Calculate individual missingness
-vcftools --vcf $OUTPUT/cranberryGBS_snps_to_impute.recode.vcf --missing-indv \
---out $OUTPUT/cranberryGBS_germplasm_missingness
-
-# Filter out individuals with missingness > 0.50
-awk 'NR > 1 { if ($5 > 0.50) print $1 }' $OUTPUT/cranberryGBS_germplasm_missingness.imiss > $OUTPUT/individual_remove_imputation.txt
-
-vcftools --vcf $OUTPUT/cranberryGBS_snps_to_impute.recode.vcf --remove $OUTPUT/individual_remove_imputation.txt \
---recode-INFO-all --recode --out $OUTPUT/cranberryGBS_snps_to_impute_filtered
+### PLINK HAPLOTYPING ###
 
 
-## Run beagle
+# First haplotyping of all germplasm
+plink --vcf $VCFIN --blocks no-pheno-req --blocks-max-kb 1000 --double-id \
+--out haplotyping/cranberryGBS_germplasm_haplotyping
 
-# Run the imputation software
-java -Xmx32g -jar /software/7/apps/beagle-geno/5.0/beagle.16May19.351.jar \
-gt=$OUTPUT/cranberryGBS_snps_to_impute_filtered.recode.vcf \
-out=$OUTPUT/cranberryGBS_germplasm_imputed_snps \
-ne=1000 burnin=6 iterations=25
+## Next haplotyping of wild accessions
+# First subset the VCF for wild germplasm and write to a plink format file
+vcftools --gzvcf $VCFIN --keep $WD/input/cranberry_gbs_wild_germplasm_individuals.txt --plink \
+--out cranberryGBS_wild_germplasm_plink
+
+# Now pass to plink
+plink --file cranberryGBS_wild_germplasm_plink --blocks no-pheno-req --blocks-min-maf 0.05 \
+--blocks-max-kb 1000 --out haplotyping/cranberryGBS_wild_germplasm_haplotypes
+
+# Remove the .ped files
+rm cranberryGBS_wild_germplasm_plink.*
 
